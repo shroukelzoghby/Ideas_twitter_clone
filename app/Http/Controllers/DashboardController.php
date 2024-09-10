1f8bbdfc779617e1500686edd915b2396c359a3f
shroukelzoghby <shrouk.elzoghby1@gmail.com>
2024-09-10 07:17:44 +0300

    initial

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Idea;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    function index()
+    {
+        $ideas = Idea::orderBy('created_at', 'desc');
+
+        if(request()->has('search'))
+        {
+            $ideas=$ideas->where('content','like','%'.request('search').'%');
+        }
+
+        return view('dashboard',['ideas' => $ideas->paginate(5)]);
+    }
+}
---
+++ app/Http/Controllers/IdeaController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Idea;
+use Illuminate\Http\Request;
+
+class IdeaController extends Controller
+{
+    public function show(Idea $idea)
+    {
+        return view('ideas.show',['idea'=>$idea]);
+    }
+    public function store(){
+
+        $validated=request()->validate([
+           'content'=>'required|min:5|max:240'
+        ]);
+
+         Idea::create($validated);
+
+        return redirect()->route('dashboard')->with('success','Idea added successfully');
+
+
+    }
+    public function edit(Idea $idea){
+        $editing=true;
+        return view('ideas.show',compact('idea','editing'));
+
+    }
+    public function update(Idea $idea){
+
+        $validated = request()->validate([
+            'content'=>'required|min:5|max:240'
+        ]);
+        $idea->update($validated);
+        return redirect()->route('ideas.show',$idea->id)->with('success','Idea updated successfully');
+    }
+
+    public function destroy(Idea $idea){
+        $idea->delete();
+        return redirect()->route('dashboard')->with('success','Idea deleted successfully');
+    }
+}
