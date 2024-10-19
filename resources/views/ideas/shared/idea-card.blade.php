5cd6c902b56215c8b21311f4f7b9f29a27366833
shroukelzoghby <shrouk.elzoghby1@gmail.com>
2024-10-19 03:02:54 +0300

    UPDATE: Update timestamps to use diffForHumans format

--- resources/views/ideas/shared/idea-card.blade.php
+++ resources/views/ideas/shared/idea-card.blade.php
@@ -29,28 +29,28 @@
                 @csrf
                 @method('put')
                 <div class="mb-3">
-                    <textarea  name= "content" class="form-control" id="content" rows="3">{{$idea->content}}</textarea>
+                    <textarea name="content" class="form-control" id="content" rows="3">{{$idea->content}}</textarea>
                     @error('content')
                     <span class="d-block fs-6 text-danger mt-2">{{$message}}</span>
                     @enderror
                 </div>
                 <div class="">
-                    <button type="submit" class="btn btn-dark mb-2 btn-sm"> Update </button>
+                    <button type="submit" class="btn btn-dark mb-2 btn-sm"> Update</button>
                 </div>
             </form>
-         @else
-        <p class="fs-6 fw-light text-muted">
-            {{$idea->content}}
-        </p>
+        @else
+            <p class="fs-6 fw-light text-muted">
+                {{$idea->content}}
+            </p>
         @endif
         <div class="d-flex justify-content-between">
             @include('ideas.shared.like-button')
             <div>
                                     <span class="fs-6 fw-light text-muted"> <span class="fas fa-clock"> </span>
-                                        {{$idea->created_at}} </span>
+                                        {{$idea->created_at->diffForHumans()}} </span>
             </div>
         </div>
-       @include('shared.comment-box')
+        @include('ideas.shared.comment-box')
     </div>
 
 </div>
